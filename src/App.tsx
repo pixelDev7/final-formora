3a1f1f0d58e15fe94903ade1d630965dd1fdb452
afgtdotcom <meaftabfct@gmail.com>
2025-07-10 08:00:54 +0530

    Fix all missing components and issues

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import RoleManagement from './pages/RoleManagement';
 import Library from './pages/Library';
 import VersionControl from './pages/VersionControl';
 import Insights from './pages/Insights';
+import Recommendations from './pages/Recommendations';
 import WorkflowBuilder from './pages/WorkflowBuilder';
 import BulkTools from './pages/BulkTools';
 import Collections from './pages/Collections';
@@ -91,6 +92,11 @@ function App() {
         <Route path="/dashboard/:formId/insights" element={
           <Layout>
             <Insights />
+          </Layout>
+        } />
+        <Route path="/recommendations/:formId" element={
+          <Layout>
+            <Recommendations />
           </Layout>
         } />
         <Route path="/workflow-builder" element={
